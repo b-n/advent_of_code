4986701a9f9cc844928b02ee8adf891f732e0f37
Ben Naylor <nayben@gmail.com>
2020-12-12 09:59:21 +0100

    Day 12! Part 2 - Now with waypoints :tm:

--- 2020/12/main.go
+++ 2020/12/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"strconv"
 	"strings"
+  "math"
+
 )
 
 func check(e error) {
@@ -18,6 +20,10 @@ func readFile(path string) (string) {
   dat, err := ioutil.ReadFile(path)
   check(err)
   return string(dat)
+}
+
+func deg2rad(deg int) float64 {
+  return float64(deg) * math.Pi / 180
 }
 
 type Ship struct {
@@ -37,6 +43,16 @@ func moveForward(s *Ship, distance int) {
   if s.dir == 180 { moveSouth(s, distance) }
   if s.dir == 270 { moveWest(s, distance) }
 }
+func rotate(s *Ship, distance int) {
+  rad := deg2rad(distance)
+  sin, cos := math.Sincos(rad)
+  cPosX, cPosY := float64(s.posX), float64(s.posY)
+  s.posX = int(math.Round((cPosX * cos) - (cPosY * sin)))
+  s.posY = int(math.Round((cPosX * sin) + (cPosY * cos)))
+}
+func rotateLeft(s *Ship, distance int) { rotate(s, distance * -1) }
+func rotateRight(s *Ship, distance int) { rotate(s, distance) }
+
 
 type movementFunction = func(s *Ship, distance int)
 
@@ -68,5 +84,24 @@ func main() {
 
   //Challenge 2
   {
+    wayPoint := Ship{posX: 10, posY: -1}
+    boatyMcBoatFace := Ship{}
+    for _, i := range instructions {
+      command := i[0]
+      distance, _ := strconv.Atoi(i[1:])
+      // move Waypoint
+      if command == 'N' || command == 'S' || command == 'E' || command == 'W' {
+        commands[command](&wayPoint, distance)
+      }
+      // move boaty
+      if command == 'F' {
+        boatyMcBoatFace.posX += wayPoint.posX * distance
+        boatyMcBoatFace.posY += wayPoint.posY * distance
+      }
+      // rotate
+      if command == 'L' { rotateLeft(&wayPoint, distance) }
+      if command == 'R' { rotateRight(&wayPoint, distance) }
+    }
+    log.Print(boatyMcBoatFace.posX + boatyMcBoatFace.posY)
   }
 }
