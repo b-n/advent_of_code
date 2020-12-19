e9e183ac22df6d6757e3d84b47f9cca24b3fd2b3
Ben Naylor <nayben@gmail.com>
2020-12-19 14:37:34 +0100

    Day 19! Part 2, Not even working! Woo!

--- 2020/19/main.go
+++ 2020/19/main.go
@@ -4,6 +4,7 @@ import (
   "io/ioutil"
   "strings"
   "strconv"
+  "regexp"
 )
 
 func check(e error) {
@@ -26,39 +27,52 @@ func safeAtoi(input string) int {
 }
 
 type Rule struct {
-  raw              [][]int
-  compiled_options []string
+  raw               [][]int
+  compiled_options  []string
 }
 
-func parseRules(input []string) map[int]Rule {
-  rules := map[int]Rule{}
+func parseRules(input []string) map[int]*Rule {
+  rules := map[int]*Rule{}
   for _, v := range input {
     parts := strings.Split(v, ": ")
     index := safeAtoi(parts[0])
-
-    if parts[1] == "\"a\"" || parts[1] == "\"b\"" {
-      rules[index] = Rule{
-        compiled_options: []string{string(parts[1][1])},
-      }
-      continue
-    }
 
     rule := Rule{
       raw: [][]int{},
       compiled_options: []string{},
     }
+    if parts[1] == "\"a\"" || parts[1] == "\"b\"" {
+      rule.compiled_options = append(rule.compiled_options, string(parts[1][1]))
+      rules[index] = &rule
+      continue
+    }
+
     for i, section := range strings.Split(parts[1], " | ") {
       rule.raw = append(rule.raw, []int{})
       for _, num := range strings.Split(section, " ") {
         rule.raw[i] = append(rule.raw[i], safeAtoi(num))
       }
     }
-    rules[index] = rule
+    rules[index] = &rule
   }
   return rules
 }
 
-func rule_options(ruleNumber int, rules map[int]Rule) []string {
+func combineArrays(input [][]string) []string {
+  res := []string{""}
+  for _, arr := range input {
+    prod :=  []string{}
+    for _, i := range arr {
+      for _, r := range res {
+        prod = append(prod, r + i)
+      }
+    }
+    res = prod
+  }
+  return res
+}
+
+func rule_options(ruleNumber int, rules map[int]*Rule) []string {
   if len(rules[ruleNumber].compiled_options) > 0 {
     return rules[ruleNumber].compiled_options
   }
@@ -72,15 +86,29 @@ func rule_options(ruleNumber int, rules map[int]Rule) []string {
       }
       continue
     }
-    for _, opt1 := range rule_options(subRule[0], rules) {
-      for _, opt2 := range rule_options(subRule[1], rules) {
-        compiled_options = append(compiled_options, opt1 + opt2)
+    rule_outputs := [][]string{}
+    infinite := false
+    for _, sub := range subRule {
+      if sub == ruleNumber { 
+        infinite = true;
+        continue
       }
+      rule_outputs = append(rule_outputs, rule_options(sub, rules))
+    }
+    if infinite {
+      for i, o := range rule_outputs {
+        for j, v := range o {
+          rule_outputs[i][j] = "(" + v + ")+"
+        }
+      }
+    }
+
+    for _, o := range combineArrays(rule_outputs) {
+      compiled_options = append(compiled_options, o)
     }
   }
   return compiled_options
 }
-
 
 func main() {
   input := readFile("./input.txt")
@@ -92,8 +120,9 @@ func main() {
   rules := parseRules(rawRules)
 
   // Challenge 1
-  {
+  if false {
     valid_options := rule_options(0, rules)
+    log.Print(len(valid_options))
     valid := 0
     for _, m := range messages {
       good := false
@@ -106,6 +135,32 @@ func main() {
   }
 
   // Challenge 2
-  {
+  if true {
+    rules[8].raw[0] = []int{42, 8}
+    rules[11].raw[0] = []int{42,11,31}
+
+    valid_options := rule_options(0, rules)
+    log.Print(len(valid_options))
+    regexs := make([]*regexp.Regexp, len(valid_options))
+    log.Print("Compiling Regexs")
+    for i, opt := range valid_options {
+      regexs[i] = regexp.MustCompile("^" + opt + "$")
+      if i % 100000 == 0 { log.Print(i) }
+    }
+
+    valid := 0
+    for c, m := range messages {
+      log.Print(c, " ", m)
+      good := false
+      for _, r := range regexs {
+        if r.MatchString(m) {
+          log.Print(m, r.String())
+          good = true
+          break
+        }
+      }
+      if good { valid++ }
+    }
+    log.Print(valid)
   }
 }
