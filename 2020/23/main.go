1e5730357bf761b91d650ce0c5f4a1cbabe089e8
Ben Naylor <nayben@gmail.com>
2020-12-23 21:01:29 +0100

    Day 23! Attempt ring + pointers, pointers everywhere

--- 2020/23/main.go
+++ 2020/23/main.go
@@ -1,28 +1,17 @@
 package main
+
 import (
+	"container/ring"
   "log"
 )
 
-func check(e error) {
-  if e != nil {
-    log.Fatal(e)
-    panic(e)
-  }
-}
-
-func wrappedPick(circle []int, from int, quantity int) []int {
-  res := circle
-  res = append(res, circle...)
-  return res[from:from+quantity]
-}
-
-func removeElements(source []int, toRemove []int) []int {
-  rMap := map[int]bool{}
-  for _, r := range toRemove { rMap[r] = true }
-  res := []int{}
-  for _, v := range source {
-    if !rMap[v] { res = append(res, v) }
-  }
+func getPickups(r *ring.Ring) []int {
+  res := make([]int, r.Len())
+  i := 0
+  r.Do(func(p interface{}) {
+    res[i] = p.(int)
+    i++
+  })
   return res
 }
 
@@ -38,71 +27,85 @@ func checkValue(arr []int, value int, maxValue int) int {
   return res
 }
 
-func play(input []int, rounds int, maxValue int) []int {
-  circle := input
+func goToInt(r *ring.Ring, value int) *ring.Ring {
+  for true {
+    if r.Value.(int) == value { break }
+    r = r.Next()
+  }
+  return r
+}
+
+func play(r *ring.Ring, positions map[int]*ring.Ring, rounds int, maxValue int) *ring.Ring {
   for i := 0; i < rounds; i++ {
-    if i % 10000 == 0 { log.Print("Round ", i) }
-    destination := circle[0] - 1
-    if (destination == 0) { destination = maxValue }
-    pickUps := circle[1:4]
+    // get our safe destination value
+    destination := r.Value.(int) - 1
+    if destination == 0 { destination = maxValue }
+
+    //get those pickups (unlink) and fix the destination value
+    pickUpRing := r.Unlink(3)
+    pickUps := getPickups(pickUpRing)
     destination = checkValue(pickUps, destination, maxValue)
 
-    res := make([]int, len(circle))
-    res[len(circle)-1] = circle[0]
-    pos := 4
-    for j := 0; j < len(circle) - 1; j++ {
-      res[j] = circle[pos]
-      if res[j] == destination {
-        res[j+1] = pickUps[0]
-        res[j+2] = pickUps[1]
-        res[j+3] = pickUps[2]
-        j += 3
-      }
-      pos++
-    }
-    circle = res
+    // where to after this?
+    nextHead := r.Next()
+
+    // find the destination number from above, and link the pickups in
+    r = positions[destination]
+    r = r.Link(pickUpRing)
+
+    // Go to the next head
+    r = nextHead
   }
-  return circle
+  return r
 }
 
 func main() {
-  //test
+  // test
   //circle := []int{3, 8, 9, 1, 2, 5, 4, 6, 7}
-  //input
+  // input
   circle := []int{6, 2, 4, 3, 9, 7, 1, 5, 8}
 
   // Challenge 1
   {
-    smallCircle := make([]int, len(circle))
-    copy(smallCircle, circle)
-    smallCircle = play(smallCircle, 100, 9)
-    res := []int{}
-    i := 0
-    for ; i < 9; i++ { if smallCircle[i] == 1 { break } }
-    i++
-    for len(res) < 8 {
-      res = append(res, smallCircle[i])
-      i++
-      i = i % 9
+    positions := map[int]*ring.Ring{}
+    r := ring.New(len(circle))
+    for _, v := range circle {
+      positions[v] = r
+      r.Value = v
+      r = r.Next()
     }
-    log.Print(res)
-  }
 
-  // Challenge 2
+    r = play(r, positions, 100, 9)
+
+    r = positions[1].Next()
+    for i := 0; i < len(circle) - 1; i++ {
+      log.Print(r.Value.(int))
+      r = r.Next()
+    }
+  }
+  // challenge 2
   {
     biggerCircle := make([]int, 1000000)
     copy(biggerCircle, circle)
     for i := 10; i <= 1000000; i++ {
       biggerCircle[i-1] = i
     }
-    log.Print(biggerCircle[:10], biggerCircle[999990:])
-    res := play(biggerCircle, 10000000, 1000000)
-    for i, v := range res {
-      if v == 1 {
-        log.Print(res[i], res[i+1], res[i+2])
-        break
-      }
+
+    positions := map[int]*ring.Ring{}
+    r := ring.New(len(biggerCircle))
+    for _, v := range biggerCircle {
+      positions[v] = r
+      r.Value = v
+      r = r.Next()
     }
-    log.Print(len(res))
+
+    r = play(r, positions, 10000000, 1000000)
+
+    r = positions[1].Next()
+    total := 1
+    total *= r.Value.(int)
+    r = r.Next()
+    total *= r.Value.(int)
+    log.Print(total)
   }
 }
