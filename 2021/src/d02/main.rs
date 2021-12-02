65ad7148d1d9725c3c8ea2f1bb4ae69ed9967b9a
Ben Naylor <nayben@gmail.com>
2021-12-02 07:35:42 +0100

    Day 2!

---
+++ 2021/src/d02/main.rs
@@ -0,0 +1,74 @@
+use std::path::Path;
+use crate::utils::*;
+
+// Learnings:
+// - I should probably learn to parse Strings to Enums
+// - String::from feels a little bit like an antipattern in the grand scheme
+// - I really should use a struct for the Vehicle and use functions on it (more fun)
+//
+// My enjoyment of rust so far: [+++-------]
+
+pub fn run() {
+    let path = Path::new("./input/02");
+
+    println!("Part 1: {}", p01(&path));
+    println!("Part 2: {}", p02(&path));
+}
+
+fn p01(p: &Path) -> i32 {
+    let lines = utils::read_to_lines(p);
+
+    let mut depth = 0;
+    let mut x = 0;
+
+    for line in lines {
+        let str_value = utils::line_as_str(line);
+
+        let (direction, distance) = str_to_command(str_value);
+
+        match direction.as_str() {
+            "forward" => x += distance,
+            "down" => depth += distance,
+            "up" => depth -= distance,
+            _ => println!("Bad command"),
+        }
+    }
+    
+    depth * x
+}
+
+fn p02(p: &Path) -> i32 {
+    let lines = utils::read_to_lines(p);
+
+    let mut depth = 0;
+    let mut x = 0;
+    let mut aim = 0;
+
+    for line in lines {
+        let str_value = utils::line_as_str(line);
+
+        let (direction, distance) = str_to_command(str_value);
+
+        match direction.as_str() {
+            "forward" => {
+                x += distance;
+                depth += aim * distance;
+            },
+            "down" => aim  += distance,
+            "up" => aim -= distance,
+            _ => println!("Bad command"),
+        }
+    }
+    
+    depth * x
+}
+
+fn str_to_command(input: String) -> (String, i32) {
+    let mut raw_command = input.split(" ");
+    // Nice to use iterators, but... coercion should be easier than a bunch of unwrap()s everywhere
+    let direction = String::from(raw_command.next().unwrap());
+    let distance = raw_command.next().unwrap().parse::<i32>().unwrap();
+
+    (direction, distance)
+}
+
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,6 +1,8 @@
-mod d01;
+//mod d01;
+mod d02;
 mod utils { pub mod utils; }
 
 fn main() {
-    d01::main::run();
+    //d01::main::run();
+    d02::main::run();
 }
