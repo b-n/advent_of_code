2446e08fe72b7be2544dd49dca9de514e27d872c
Ben Naylor <nayben@gmail.com>
2021-12-04 11:58:32 +0100

    Day 4 Part 2!

--- 2021/src/d04/main.rs
+++ 2021/src/d04/main.rs
@@ -1,11 +1,19 @@
 use crate::utils::file;
 use std::path::Path;
 
+// Learnings:
+// - I should use the auto formatter more often (thus the small changes in this commit)
+// - Structs and functions aren't that bad really. Reminds me a lot of golang (what came first?)
+// - Advent of code ramps up in the weekend (at least it's not during the week)
+// - Iterators are "good". I should use them a lot more often in other languages. Efficient
+//
+// Rust Enjoyment factor: [+++/------] (+ 1/2, this problem makes it interesting I guess)
+
 pub fn run() {
     let path = Path::new("./input/04");
 
     println!("Part 1: {}", p01(&path).unwrap());
-    //println!("Part 2: {}", p02(&path).unwrap());
+    println!("Part 2: {}", p02(&path).unwrap());
 }
 
 type BingoBoard = Vec<Vec<usize>>;
@@ -14,32 +22,36 @@ type BingoBoard = Vec<Vec<usize>>;
 struct BingoGame {
     numbers: Vec<usize>,
     players: Vec<BingoPlayer>,
+    playing: usize,
 }
 
 #[derive(Debug)]
 struct BingoPlayer {
+    won: bool,
     board: BingoBoard,
 }
 
 impl BingoPlayer {
     fn new(board: BingoBoard) -> Self {
-        Self { board }
+        Self { won: false, board }
     }
 
     fn take(&mut self, num: usize) -> Option<bool> {
-        let mut pos: (usize, usize) = (25,24);
+        let mut pos: (usize, usize) = (25, 24);
         let mut halting = false;
 
         //find the value, if we find it, mark it done (100 = out of bounds)
         for (y, row) in self.board.iter_mut().enumerate() {
             for (x, val) in row.iter_mut().enumerate() {
                 if *val == num {
-                    pos = (y,x);
+                    pos = (y, x);
                     *val = 100;
                     halting = true;
                 }
             }
-            if halting { break; }
+            if halting {
+                break;
+            }
         }
 
         //self.print_board();
@@ -56,14 +68,19 @@ impl BingoPlayer {
             won = col || row;
         }
 
+        self.won = won;
+
         Some(won)
     }
 
     fn get_board_sum(&self) -> Option<usize> {
-        Some(self.board.iter()
-            .flatten()
-            .filter(|&x| *x < 100)
-            .sum::<usize>())
+        Some(
+            self.board
+                .iter()
+                .flatten()
+                .filter(|&x| *x < 100)
+                .sum::<usize>(),
+        )
     }
 
     #[allow(dead_code)]
@@ -79,13 +96,42 @@ fn p01(p: &Path) -> Option<usize> {
     let mut game = bingo_game_from_path(p)?;
 
     for num in game.numbers.iter() {
-
         for player in game.players.iter_mut() {
             if player.take(*num)? {
                 return Some(num * player.get_board_sum()?);
             }
         }
     }
+    Some(0)
+}
+
+fn p02(p: &Path) -> Option<usize> {
+    let mut game = bingo_game_from_path(p)?;
+
+    let mut last_player = usize::MAX;
+    for num in game.numbers.iter() {
+        for player in game.players.iter_mut() {
+            if !player.won && player.take(*num)? {
+                game.playing -= 1
+            }
+        }
+
+        // store the last player, but keep playing until they are finished
+        if game.playing == 1 {
+            last_player = game
+                .players
+                .iter()
+                .enumerate()
+                .filter(|(_, player)| !player.won)
+                .map(|(index, _)| index)
+                .collect::<Vec<usize>>()[0];
+        }
+
+        if game.playing == 0 {
+            return Some(game.players[last_player].get_board_sum()? * num);
+        }
+    }
+
     Some(0)
 }
 
@@ -107,7 +153,11 @@ fn bingo_game_from_path(p: &Path) -> Option<BingoGame> {
         players.push(BingoPlayer::new(board));
     }
 
-    Some(BingoGame { numbers, players })
+    Some(BingoGame {
+        playing: players.len(),
+        numbers,
+        players,
+    })
 }
 
 fn bingo_line_to_vec(s: String) -> Option<Vec<usize>> {
