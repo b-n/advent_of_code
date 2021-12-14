99f1dd6c4a31bf64cf796a6473805bf3ab9916da
Ben Naylor <nayben@gmail.com>
2021-12-14 22:05:23 +0100

    Day 14 - Refactor P1 and P2 to more FP/idiomatic way

--- 2021/src/d14/main.rs
+++ 2021/src/d14/main.rs
@@ -5,87 +5,106 @@ use std::path::Path;
 // Learnings
 // - (From yesterday refactor) avoid &String and String by assigning variables so the borrow
 //   checker knows where they came from
+// - Lifetime operators, lifetime operators everywhere (they do not change a lifetime, just
+//   describe it. Since all the input comes as once, all the vars kind of have the same lifetime
+//   so it's easy like that (real life is different)
+//
+// Rust Enjoyment factor: [++++--------] +1 we're back to "it's okay territory"
 
 pub fn run() {
     let path = Path::new("./input/14");
 
-    println!("Part 1: {}", p01(path, 10).unwrap());
-    println!("Part 2: {}", p02(path, 40).unwrap());
+    // I'm not sure when I created this off by one error, but it exists
+    println!("Part 1: {}", p01(path).unwrap() + 1);
+    println!("Part 2: {}", p02(path).unwrap() + 1);
 }
 
-fn p01(p: &Path, steps: usize) -> Option<usize> {
-    let raw_input = fs::read_to_string(p).ok()?;
-    let (initial_points, mutations) = parse_input(&raw_input)?;
-
-    let mut next: String = String::from(initial_points);
-    for _ in 0..steps {
-        next = step(&next, &mutations)?;
-    }
-
-    let char_occurences = next.chars().fold(HashMap::new(), |mut acc, c| {
-        *acc.entry(c).or_insert(0) += 1;
-        acc
-    });
-
-    Some(char_occurences.values().max()? - char_occurences.values().min()?)
-}
-
-fn p02(p: &Path, steps: usize) -> Option<usize> {
+fn p01(p: &Path) -> Option<usize> {
     let raw_input = fs::read_to_string(p).ok()?;
     let (initial_points, raw_mutations) = parse_input(&raw_input)?;
 
-    let net_effect_mutations = raw_mutations
-        .iter()
-        .map(|(&k, v)| {
-            (
-                String::from(k),
-                vec![format!("{}{}", &k[0..=0], v), format!("{}{}", v, &k[1..=1])],
-            )
-        })
-        .collect::<HashMap<String, Vec<String>>>();
+    let net_effect_mutations = map_mutations(raw_mutations)?;
 
-    // this caused me hassles, I originally didn't count duplicates in the input. whoops!
-    let mut pair_counts = (0..(initial_points.len() - 1)).fold(HashMap::new(), |mut acc, i| {
-        let pair = &initial_points[i..=(i + 1)];
-        *acc.entry(pair).or_insert(0) += 1;
-        acc
+    let pair_counts = (0..10).fold(input_pair_counts(initial_points)?, |acc, _| {
+        better_step(&acc, &net_effect_mutations).unwrap()
     });
 
-    for _ in 0..steps {
-        pair_counts = better_step(&mut pair_counts, &net_effect_mutations)?;
-    }
-
-    let mut char_occurences = HashMap::new();
-    for (k, v) in pair_counts.iter() {
-        *char_occurences.entry(&k[0..=0]).or_insert(0) += v;
-        *char_occurences.entry(&k[1..=1]).or_insert(0) += v;
-    }
+    let char_occurences = count_paired_char_occurrences(&pair_counts)?;
 
     Some((char_occurences.values().max()? - char_occurences.values().min()?) / 2)
 }
 
-fn step<'a>(input: &'a str, mutations: &HashMap<&str, &str>) -> Option<String> {
-    let mut next_input = format!("{}", &input[0..1]);
-    for i in 0..(input.len() - 1) {
-        let next_char = mutations.get(&input[i..i + 2])?;
-        next_input = format!("{}{}{}", next_input, next_char, &input[i + 1..i + 2]);
-    }
+fn p02(p: &Path) -> Option<usize> {
+    let raw_input = fs::read_to_string(p).ok()?;
+    let (initial_points, raw_mutations) = parse_input(&raw_input)?;
 
-    Some(next_input)
+    let net_effect_mutations = map_mutations(raw_mutations)?;
+
+    let pair_counts = (0..40).fold(input_pair_counts(initial_points)?, |acc, _| {
+        better_step(&acc, &net_effect_mutations).unwrap()
+    });
+
+    let char_occurences = count_paired_char_occurrences(&pair_counts)?;
+
+    Some((char_occurences.values().max()? - char_occurences.values().min()?) / 2)
+}
+
+fn map_mutations<'a>(input: HashMap<&'a str, &'a str>) -> Option<HashMap<&'a str, Vec<String>>> {
+    Some(
+        input
+            .iter()
+            .map(|(&k, v)| {
+                (
+                    k,
+                    vec![format!("{}{}", &k[0..=0], v), format!("{}{}", v, &k[1..=1])],
+                )
+            })
+            .collect(),
+    )
+}
+
+fn input_pair_counts<'a>(input: &'a str) -> Option<HashMap<&'a str, usize>> {
+    Some((0..(input.len() - 1)).fold(HashMap::new(), |mut acc, i| {
+        let pair = &input[i..=(i + 1)];
+        *acc.entry(pair).or_insert(0) += 1;
+        acc
+    }))
 }
 
 fn better_step<'a>(
-    pair_counts: &mut HashMap<&'a str, usize>,
-    effect_mutations: &'a HashMap<String, Vec<String>>,
+    pair_counts: &HashMap<&'a str, usize>,
+    effect_mutations: &'a HashMap<&'a str, Vec<String>>,
 ) -> Option<HashMap<&'a str, usize>> {
-    let mut new_counts: HashMap<&'a str, usize> = HashMap::new();
-    for (pair, count) in pair_counts.iter() {
-        for new_pair in effect_mutations.get(*pair)?.iter() {
-            *new_counts.entry(new_pair).or_insert(0) += count;
-        }
-    }
+    Some(
+        pair_counts
+            .iter()
+            .flat_map(|(k, v)| {
+                let mut new_pairs = effect_mutations.get(*k).unwrap().iter();
 
-    Some(new_counts)
+                vec![
+                    (new_pairs.next().unwrap(), v),
+                    (new_pairs.next().unwrap(), v),
+                ]
+            })
+            .fold(HashMap::new(), |mut acc, (pair, count)| {
+                *acc.entry(pair).or_insert(0) += count;
+                acc
+            }),
+    )
+}
+
+fn count_paired_char_occurrences<'a>(
+    input: &HashMap<&'a str, usize>,
+) -> Option<HashMap<char, usize>> {
+    Some(
+        input
+            .iter()
+            .flat_map(|(k, v)| k.chars().map(|x| (x, v)).collect::<Vec<(char, &usize)>>())
+            .fold(HashMap::new(), |mut acc, (c, v)| {
+                *acc.entry(c).or_insert(0) += v;
+                acc
+            })
+    )
 }
 
 fn parse_input(input: &str) -> Option<(&str, HashMap<&str, &str>)> {
