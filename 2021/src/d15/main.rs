520d14b0b5e2d034685c301fd9c2f7f9f8c7570f
Ben Naylor <nayben@gmail.com>
2021-12-15 15:50:35 +0100

    Playing with cost algos, and making viz a little better/easier

--- 2021/src/d15/main.rs
+++ 2021/src/d15/main.rs
@@ -12,7 +12,6 @@ pub fn run() {
 const CARDINAL_DIRS: &'static [(i32, i32)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
 
 type Point = (usize, usize);
-
 type Cost = usize;
 
 fn p01(p: &Path) -> Option<usize> {
@@ -30,17 +29,16 @@ fn p01(p: &Path) -> Option<usize> {
 
     let mut next: HashMap<Point, Cost> = next_points(&(0, 0), 0, &grid, in_bounds, &mut point_cost);
 
-    //println!("{}[2J", 27 as char);
+    println!("{}[2J", 27 as char);
     while next.len() > 0 {
-        let mut sorted_points: Vec<(Point, Cost)> = next.iter()
-            .map(|(p, cost)| (*p, *cost))
-            .collect();
+        let mut sorted_points: Vec<(Point, Cost)> =
+            next.iter().map(|(p, cost)| (*p, *cost)).collect();
         sorted_points.sort_by(|a, b| a.1.cmp(&b.1));
 
         let (point, cost) = sorted_points.first()?;
         next.remove(point);
 
-        //print!("{}[2j{}[{};{}H{}", 27 as char, 27 as char, point.0, point.1, cost % 10);
+        print_pos(point, &point_cost);
 
         for (point, cost) in next_points(point, *cost, &grid, in_bounds, &mut point_cost) {
             point_cost.insert(point, cost);
@@ -72,8 +70,10 @@ fn p02(p: &Path) -> Option<usize> {
                 let new_y = y + i * height;
                 let new_x = x + j * width;
                 let mut cost = cost + i + j;
-                if cost > 9 { cost -= 9 }
-                grid.insert((new_y, new_x), cost); 
+                if cost > 9 {
+                    cost -= 9
+                }
+                grid.insert((new_y, new_x), cost);
             }
         }
     }
@@ -87,18 +87,32 @@ fn p02(p: &Path) -> Option<usize> {
     point_cost.insert((0, 0), 0);
 
     let mut next: HashMap<Point, Cost> = next_points(&(0, 0), 0, &grid, in_bounds, &mut point_cost);
+    
+    //let f_factor: usize = 10;
+    let f_score = |p: &Point, cost: Cost| {
+        // A failed attempt at euclidean distance
+        //let y = grid_max_y - p.0;
+        //let x = grid_max_x - p.1;
+        //((y * y * 1000 + x * x * 1000) as f64).sqrt() as usize + cost * f_factor
+        cost
+    };
 
-    //println!("{}[2J", 27 as char);
+    println!("{}[2J", 27 as char);
+    let end = &(*grid_max_y, *grid_max_x);
     while next.len() > 0 {
-        let mut sorted_points: Vec<(Point, Cost)> = next.iter()
-            .map(|(p, cost)| (*p, *cost))
-            .collect();
-        sorted_points.sort_by(|a, b| a.1.cmp(&b.1));
+        if point_cost.contains_key(end) {
+            break;
+        }
+        let mut sorted_points: Vec<(Point, Cost, Cost)> =
+            next.iter().map(|(p, cost)| {
+                (*p, *cost, f_score(p, *cost))
+            }).collect();
+        sorted_points.sort_by(|a, b| a.2.cmp(&b.2));
 
-        let (point, cost) = sorted_points.first()?;
+        let (point, cost, _) = sorted_points.first()?;
         next.remove(point);
 
-        //print!("{}[2j{}[{};{}H{}", 27 as char, 27 as char, point.0, point.1, cost % 10);
+        print_pos(point, &point_cost);
 
         for (point, cost) in next_points(point, *cost, &grid, in_bounds, &mut point_cost) {
             point_cost.insert(point, cost);
@@ -110,14 +124,36 @@ fn p02(p: &Path) -> Option<usize> {
     }
 
     let start = &(0, 0);
-    let end = &(*grid_max_y, *grid_max_x);
 
     Some(*point_cost.get(end)? - *point_cost.get(start)?)
 }
 
+fn print_pos(p: &Point, costs: &HashMap<Point, Cost>) {
+    print!(
+        "{}[2j{}[{};{}H{}",
+        27 as char,
+        27 as char,
+        p.0 / 2,
+        p.1,
+        display_char(p, costs)
+    );
+}
+
+fn display_char((y, x): &Point, costs: &HashMap<Point, Cost>) -> char {
+    match if y % 2 == 0 {
+        2 & (costs.contains_key(&(y + 1, *x)) as usize & 1)
+    } else {
+        1 & (costs.contains_key(&(y - 1, *x)) as usize & 2)
+    } {
+        1 => '▀',
+        2 => '▄',
+        _ => '█',
+    }
+}
+
 fn next_points<F>(
     from: &Point,
-    cost: usize,
+    cost: Cost,
     chart: &HashMap<Point, Cost>,
     in_bounds: F,
     point_costs: &mut HashMap<Point, Cost>,
@@ -132,7 +168,9 @@ where
     // we shouldn't return a point if we know it's going to cost more than we currently have for
     // that point, that's just dumb
 
-    let is_cheaper = |(p, cost): &(Point, usize)| !point_costs.contains_key(p) || point_costs.get(p).unwrap() > cost;
+    let is_cheaper = |(p, cost): &(Point, Cost)| {
+        !point_costs.contains_key(p) || point_costs.get(p).unwrap() > cost
+    };
 
     CARDINAL_DIRS
         .iter()
@@ -153,7 +191,7 @@ fn parse_input(input: &str) -> Option<HashMap<Point, Cost>> {
                 line.chars()
                     .enumerate()
                     .map(|(x, c)| ((y, x), (c as usize) - ('0' as usize)))
-                    .collect::<Vec<(Point, usize)>>()
+                    .collect::<Vec<(Point, Cost)>>()
             })
             .collect(),
     )
