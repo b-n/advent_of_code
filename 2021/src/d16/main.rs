ce2095d906ac5b797a647d26ab08a91bfd313038
Ben Naylor <nayben@gmail.com>
2021-12-16 17:42:15 +0100

    D16 Working P1, WIP P2

--- 2021/src/d16/main.rs
+++ 2021/src/d16/main.rs
@@ -1,4 +1,3 @@
-//use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
@@ -6,18 +5,19 @@ pub fn run() {
     let path = Path::new("./input/16");
 
     println!("Part 1: {}", p01(path).unwrap());
-    //println!("Part 2: {}", p02(path).unwrap());
+    println!("Part 2: {}", p02(path).unwrap());
 }
 
 #[derive(Debug)]
 struct Literal {
     version: u64,
-    numbers: Vec<u8>
+    value: u64,
 }
 
 #[derive(Debug)]
 struct Operator {
     version: u64,
+    t: u64,
     operators: Vec<Operator>,
     literals: Vec<Literal>,
 }
@@ -33,22 +33,35 @@ impl Operator {
         }
         versions
     }
+
+    fn items<'a>(&'a self) -> impl Iterator<Item = u64> + 'a {
+         self.literals
+             .iter()
+             .map(|x| x.value)
+             .chain(self.operators.iter().map(|o| o.run()))
+    }
+
+    fn run(&self) -> u64 {
+        match self.t {
+            0 => {
+                self.items().sum::<u64>()
+            }
+            //1 => {
+            //self.literals.iter().map(|x| x.value).sum::<u64>()
+            //+ self.operators.iter().map(|o| o.run()).sum::<u64>()
+            //},
+            _ => 0,
+        }
+    }
 }
 
 fn p01(p: &Path) -> Option<usize> {
-    //let raw_input = fs::read_to_string(p).ok()?;
-    //let raw_input = "8A004A801A8002F478";
-    //let raw_input = "620080001611562C8802118E34";
-    let raw_input = "C0015000016115A2E0802F182340";
-    //let raw_input = "A0016C880162017C3686B18A3D4780";
-    //let raw_input = "38006F45291200";
-    //let raw_input = "D2FE28";
+    let raw_input = fs::read_to_string(p).ok()?;
 
-    let mut bit_stack: Vec<u8> = raw_input.chars()
+    let mut bit_stack: Vec<u8> = raw_input
+        .chars()
         .filter(|x| x != &'\n')
-        .map(|x| {
-            u64::from_str_radix(&format!("{}", x), 16).unwrap() as u8
-        })
+        .map(|x| u64::from_str_radix(&format!("{}", x), 16).unwrap() as u8)
         .collect();
 
     let mut literals: Vec<Literal> = vec![];
@@ -73,75 +86,113 @@ fn p01(p: &Path) -> Option<usize> {
     Some(versions as usize)
 }
 
+fn p02(p: &Path) -> Option<usize> {
+    let raw_input = fs::read_to_string(p).ok()?;
+
+    let mut bit_stack: Vec<u8> = raw_input
+        .chars()
+        .filter(|x| x != &'\n')
+        .map(|x| u64::from_str_radix(&format!("{}", x), 16).unwrap() as u8)
+        .collect();
+
+    let (_, _, operator) = get_packet(&mut bit_stack)?;
+
+    //let mut versions: u64 = 0;
+    //for l in literals.iter() {
+        //versions += l.version;
+    //}
+    //for o in operators.iter() {
+        //versions += o.get_versions();
+    //}
+
+    Some(operator?.run() as usize)
+}
+
 fn get_packet(bit_stack: &mut Vec<u8>) -> Option<(u64, Option<Literal>, Option<Operator>)> {
     let version = get_bits(3, bit_stack);
     let t = get_bits(3, bit_stack);
-    
+
     if t == 4 {
         let (used_bits, literal) = get_literal(version, bit_stack)?;
         Some((used_bits, Some(literal), None))
     } else {
-        let (used_bits, operator) = get_operator(version, bit_stack)?;
+        let (used_bits, operator) = get_operator(version, t, bit_stack)?;
         Some((used_bits, None, Some(operator)))
     }
 }
 
 fn get_literal(version: u64, bit_stack: &mut Vec<u8>) -> Option<(u64, Literal)> {
-    println!("literal");
-    let mut numbers = vec![];
+    //let mut numbers = vec![];
+    let mut value = 0;
     let mut used_bits = 6;
     loop {
         let num = get_bits(5, bit_stack);
-        numbers.push((num & 15) as u8);
+        value <<= 4;
+        value |= num & 15;
+        //numbers.push((num & 15) as u8);
         used_bits += 5;
         if num & 16 != 16 {
-            break
+            break;
         }
     }
-    Some((used_bits, Literal { version, numbers }))
+    Some((used_bits, Literal { version, value }))
 }
 
-fn get_operator(version: u64, bit_stack: &mut Vec<u8>) -> Option<(u64, Operator)> {
-    println!("operator");
+fn get_operator(version: u64, t: u64, bit_stack: &mut Vec<u8>) -> Option<(u64, Operator)> {
     let length_type = get_bits(1, bit_stack);
     let mut literals = vec![];
     let mut operators = vec![];
     let mut used_bits = 7;
-    if length_type == 0 {
-        let mut total_length = get_bits(15, bit_stack);
-        used_bits += total_length;
-        println!("length_type: 0, {:?}", total_length);
-        while total_length > 0 {
-            println!("yo: {}",  total_length);
-            match get_packet(bit_stack)? {
-                (i, Some(l), None) => { literals.push(l); total_length -= i as u64; },
-                (i, None, Some(o)) => { operators.push(o); total_length -= i as u64; },
-                _ => ()
-            }
-        } 
-    } else {
-        let sub_packet_count = get_bits(11, bit_stack);
-        used_bits += 11;
-        println!("length_type: 1, {:?}", sub_packet_count);
-
-        for _ in 0..sub_packet_count {
-            match get_packet(bit_stack)? {
-                (i, Some(l), None) => { literals.push(l); used_bits += i as u64; },
-                (i, None, Some(o)) => { operators.push(o); used_bits += i as u64; },
-                _ => ()
+    match length_type {
+        0 => {
+            let mut total_length = get_bits(15, bit_stack);
+            used_bits += 15;
+            used_bits += total_length;
+            while total_length > 0 {
+                total_length -= match get_packet(bit_stack)? {
+                    (i, Some(l), None) => {
+                        literals.push(l);
+                        i as u64
+                    }
+                    (i, None, Some(o)) => {
+                        operators.push(o);
+                        i as u64
+                    }
+                    _ => 0,
+                }
             }
         }
+        1 => {
+            let sub_packet_count = get_bits(11, bit_stack);
+            used_bits += 11;
+
+            for _ in 0..sub_packet_count {
+                used_bits += match get_packet(bit_stack)? {
+                    (i, Some(l), None) => {
+                        literals.push(l);
+                        i as u64
+                    }
+                    (i, None, Some(o)) => {
+                        operators.push(o);
+                        i as u64
+                    }
+                    _ => 0,
+                }
+            }
+        }
+        _ => (),
     }
-    
-    Some((used_bits, Operator{ version, literals, operators }))
+
+    Some((
+        used_bits,
+        Operator {
+            version,
+            t,
+            literals,
+            operators,
+        },
+    ))
 }
-
-//fn p02(p: &Path) -> Option<usize> {
-    //let raw_input = fs::read_to_string(p).ok()?;
-
-    //Some(0)
-//}
-
 
 fn get_bits(size: usize, hexes: &mut Vec<u8>) -> u64 {
     let mut output: u64 = 0;
@@ -163,12 +214,12 @@ fn get_bits(size: usize, hexes: &mut Vec<u8>) -> u64 {
         for i in 0..hexes.len() {
             hexes[i] = (hexes[i] << to_get) & 15;
             if i < hexes.len() - 1 {
-                hexes[i] |= hexes[i+1] >> (4 - to_get);
+                hexes[i] |= hexes[i + 1] >> (4 - to_get);
             }
         }
 
         if hexes[hexes.len() - 1] == 0 {
-            hexes.remove(hexes.len() -1);
+            hexes.remove(hexes.len() - 1);
         }
 
         to_get = 0;
