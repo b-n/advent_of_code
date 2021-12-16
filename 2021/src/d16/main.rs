469904df4d92e751047ddc88b384b643dee2d818
Ben Naylor <b.naylor@catawiki.nl>
2021-12-16 16:47:43 +0100

    D16 Small progress!

--- 2021/src/d16/main.rs
+++ 2021/src/d16/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+//use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
 pub fn run() {
-    let path = Path::new("./input/16_example");
+    let path = Path::new("./input/16");
 
     println!("Part 1: {}", p01(path).unwrap());
     //println!("Part 2: {}", p02(path).unwrap());
@@ -11,62 +11,129 @@ pub fn run() {
 
 #[derive(Debug)]
 struct Literal {
-    version: u8,
+    version: u64,
     numbers: Vec<u8>
+}
+
+#[derive(Debug)]
+struct Operator {
+    version: u64,
+    operators: Vec<Operator>,
+    literals: Vec<Literal>,
+}
+
+impl Operator {
+    fn get_versions(&self) -> u64 {
+        let mut versions = self.version;
+        for l in self.literals.iter() {
+            versions += l.version;
+        }
+        for o in self.operators.iter() {
+            versions += o.get_versions();
+        }
+        versions
+    }
 }
 
 fn p01(p: &Path) -> Option<usize> {
     //let raw_input = fs::read_to_string(p).ok()?;
     //let raw_input = "8A004A801A8002F478";
-    let raw_input = "D2FE28";
+    //let raw_input = "620080001611562C8802118E34";
+    let raw_input = "C0015000016115A2E0802F182340";
+    //let raw_input = "A0016C880162017C3686B18A3D4780";
+    //let raw_input = "38006F45291200";
+    //let raw_input = "D2FE28";
 
-    let mut hexes: Vec<u8> = raw_input.chars()
+    let mut bit_stack: Vec<u8> = raw_input.chars()
+        .filter(|x| x != &'\n')
         .map(|x| {
             u64::from_str_radix(&format!("{}", x), 16).unwrap() as u8
         })
         .collect();
 
     let mut literals: Vec<Literal> = vec![];
+    let mut operators: Vec<Operator> = vec![];
 
-    while !hexes.is_empty() {
-        let version = get_bits(3, &mut hexes);
-        let t = get_bits(3, &mut hexes);
-
-        println!("{:#b} {:#b}", version, t);
-
-        if t == 4 {
-            let mut numbers = vec![];
-            let mut used_bits = 6;
-            loop {
-                let num = get_bits(5, &mut hexes);
-                numbers.push((num & 15) as u8);
-                used_bits += 5;
-                if num & 16 != 16 {
-                    break
-                }
-            }
-            println!("{:?}", numbers);
-            println!("draining {} {}", used_bits, used_bits % 4);
-            if used_bits % 4 > 0 {
-                get_bits(used_bits % 4, &mut hexes);
-            }
-
-            literals.push(Literal { version: version as u8, numbers })
+    while !bit_stack.is_empty() {
+        match get_packet(&mut bit_stack)? {
+            (_, Some(l), None) => literals.push(l),
+            (_, None, Some(o)) => operators.push(o),
+            _ => (),
         }
     }
 
-    println!("{:?}", literals);
-      
+    let mut versions: u64 = 0;
+    for l in literals.iter() {
+        versions += l.version;
+    }
+    for o in operators.iter() {
+        versions += o.get_versions();
+    }
 
-    //    100 = literal value
-    // VVVTTT000000000000000
-    //   !000 = operator value
-    //
-    //    110 = not 100, so operator
-    // VVVTTTILLLLLLLLLLLLLLLLLLL<SUB PACKET>
-    //       I = 1 or 0 (11 or 15 bit length L
+    Some(versions as usize)
+}
 
-    Some(0)
+fn get_packet(bit_stack: &mut Vec<u8>) -> Option<(u64, Option<Literal>, Option<Operator>)> {
+    let version = get_bits(3, bit_stack);
+    let t = get_bits(3, bit_stack);
+    
+    if t == 4 {
+        let (used_bits, literal) = get_literal(version, bit_stack)?;
+        Some((used_bits, Some(literal), None))
+    } else {
+        let (used_bits, operator) = get_operator(version, bit_stack)?;
+        Some((used_bits, None, Some(operator)))
+    }
+}
+
+fn get_literal(version: u64, bit_stack: &mut Vec<u8>) -> Option<(u64, Literal)> {
+    println!("literal");
+    let mut numbers = vec![];
+    let mut used_bits = 6;
+    loop {
+        let num = get_bits(5, bit_stack);
+        numbers.push((num & 15) as u8);
+        used_bits += 5;
+        if num & 16 != 16 {
+            break
+        }
+    }
+    Some((used_bits, Literal { version, numbers }))
+}
+
+fn get_operator(version: u64, bit_stack: &mut Vec<u8>) -> Option<(u64, Operator)> {
+    println!("operator");
+    let length_type = get_bits(1, bit_stack);
+    let mut literals = vec![];
+    let mut operators = vec![];
+    let mut used_bits = 7;
+    if length_type == 0 {
+        let mut total_length = get_bits(15, bit_stack);
+        used_bits += total_length;
+        println!("length_type: 0, {:?}", total_length);
+        while total_length > 0 {
+            println!("yo: {}",  total_length);
+            match get_packet(bit_stack)? {
+                (i, Some(l), None) => { literals.push(l); total_length -= i as u64; },
+                (i, None, Some(o)) => { operators.push(o); total_length -= i as u64; },
+                _ => ()
+            }
+        } 
+    } else {
+        let sub_packet_count = get_bits(11, bit_stack);
+        used_bits += 11;
+        println!("length_type: 1, {:?}", sub_packet_count);
+
+        for _ in 0..sub_packet_count {
+            match get_packet(bit_stack)? {
+                (i, Some(l), None) => { literals.push(l); used_bits += i as u64; },
+                (i, None, Some(o)) => { operators.push(o); used_bits += i as u64; },
+                _ => ()
+            }
+        }
+    }
+    
+    Some((used_bits, Operator{ version, literals, operators }))
 }
 
 //fn p02(p: &Path) -> Option<usize> {
@@ -80,26 +147,24 @@ fn get_bits(size: usize, hexes: &mut Vec<u8>) -> u64 {
     let mut output: u64 = 0;
     let mut to_get = size.clone();
 
-    //println!("get_bits {}", to_get);
     while to_get > 0 {
-        //println!("{} {}", to_get, output);
+        if hexes.len() == 0 {
+            to_get = 0;
+            continue;
+        }
+
         if to_get >= 4 {
             output = (output << 4) | (hexes.remove(0) as u64);
             to_get -= 4;
             continue;
         }
-        //println!("{} {:#b}", output, hexes[0] >> 4 - to_get);
         output = (output << to_get) | (hexes[0] >> (4 - to_get)) as u64;
 
         for i in 0..hexes.len() {
-            //println!("{:#b} {:#b} {:#b}", hexes[i], hexes[i] << to_get, (hexes[i] << to_get) & 7);
-
             hexes[i] = (hexes[i] << to_get) & 15;
-            //println!("{:#b}", hexes[i]);
             if i < hexes.len() - 1 {
                 hexes[i] |= hexes[i+1] >> (4 - to_get);
             }
-            //println!("end shifting {:#b}", hexes[i]);
         }
 
         if hexes[hexes.len() - 1] == 0 {
@@ -108,6 +173,5 @@ fn get_bits(size: usize, hexes: &mut Vec<u8>) -> u64 {
 
         to_get = 0;
     }
-    //println!("{}", output);
     output
 }
