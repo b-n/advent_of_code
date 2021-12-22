1190e802f23e6276e4d7e2dbf2d648e2a82364b6
Ben Naylor <nayben@gmail.com>
2021-12-22 08:09:26 +0100

    D22 P2 plan!

--- 2021/src/d22/main.rs
+++ 2021/src/d22/main.rs
@@ -1,15 +1,15 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeSet};
 use std::fs;
 use std::path::Path;
 
 pub fn run() {
     let path = Path::new("./input/22");
 
-    println!("Part 1: {}", p01(path).unwrap());
-    //println!("Part 2: {}", p02(path).unwrap());
+    //println!("Part 1: {}", p01(path).unwrap());
+    println!("Part 2: {}", p02(path).unwrap());
 }
 
-#[derive(Debug, Hash, Eq, PartialEq)]
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 struct Point3d {
     x: i64,
     y: i64,
@@ -81,9 +81,16 @@ impl Cuboid {
                 .flat_map(move |y| (self.p1.z..=self.p2.z).map(move |z| Point3d { x, y, z }))
         })
     }
+
+    pub fn contains(&self, other: &Self) -> bool{
+        other.p1.x >= self.p1.x && other.p1.x <= self.p2.x
+        && other.p1.y >= self.p1.y && other.p1.y <= self.p2.y
+        && other.p1.z >= self.p1.z && other.p1.z <= self.p2.z
+    }
 }
 
 fn p01(p: &Path) -> Option<usize> {
+    // This naive approach eats through 16GB ram on P2 in 1 minute
     let raw_input = fs::read_to_string(p).ok()?;
 
     let cuboids = raw_input
@@ -105,4 +112,33 @@ fn p01(p: &Path) -> Option<usize> {
     }
 
     Some(chart.len())
+}
+
+fn p02(p: &Path) -> Option<usize> {
+    let raw_input = fs::read_to_string(p).ok()?;
+
+    let cuboids = raw_input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse::<Cuboid>().unwrap())
+        .collect::<Vec<Cuboid>>();
+
+
+    // What we know, the number of on/off bits is (x.1 - x.0) * (y.1 - y.0) * (z.1 -z.0)
+    // if an existing cube is contained entirely inside a new cube, then we can kill the existing
+    // The new cube is always taken in it's entirety
+    //   - We can drop off cubes, after we cut the existing cubes
+    // If any existing cubes are in the way, we need to cut them
+    //   - new cube intersects a face of an existing cube, that cube is shrunk on that face
+    //   - new cube protrudes into the existing cube, existing cube is split
+    //   - the point of intersection determines the amount of new cubes
+    //   - At most, the existing cube would be split into 26 cubes (where the new cube fits
+    //     entirely into an existing cube
+    // In the end, we just go through all cubes and sum
+    //
+    // What we need:
+    //   - An contains and/or intersection function (so we know if we need to do anything)
+    //   - A splitting type function
+
+    Some(0)
 }
